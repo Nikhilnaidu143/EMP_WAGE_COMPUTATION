cde3a66a84816ad3ed74a9875579aaf4c1cad5f2
saitarun135 <saitarun135@gm.com>
2021-11-02 17:16:45 +0530

    UC-2:- Calculated daily wage.

--- empWageProblem.java
+++ empWageProblem.java
@@ -1,18 +1,28 @@
-/* UC-1 check employee is present or absent.*/
+/* UC-2:- Calculate daily employee wage */
 public class empWageProblem {
         public static void main(String[] args) {
                 /* Displaying welcome message in masterbranch. */
                 System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM.\n");
 
+                /*Constant Variables.*/
                 int isPresent = 1;
-                                         /*employee check */
-                double empCheck = Math.floor(Math.random()*10%2);
+                int empRatePHr = 20;
 
+                /*Temporary Variables */
+                int empHrs = 0;
+                int empWage = 0;
+
+                /* Finding employee is present or absent */
+                double empCheck = Math.floor(Math.random()*10%2);
                 if(empCheck == isPresent) {
-                        System.out.println("EMPLOYEE IS PRESENT.");
+                        empHrs = 8;
                 }
                 else {
-                        System.out.println("EMPLOYEE IS ABSENT.");
+                        empHrs = 0;
                 }
+                /* Calculate employee daily wage */
+                empWage = empHrs * empRatePHr;
+                System.out.println("EMPLOYEE DAILY WAGE :- " + empWage + "\n");
         }
 }
+
