59d7dad9fb583c2ca114b7e628bec8163c3799e1
saitarun135 <saitarun135@gm.com>
2021-11-02 17:09:09 +0530

    START:- Displayed welcome message on master branch.

---
+++ empWageProblem.java
@@ -0,0 +1,7 @@
+/*START:- displaying welcome mssg on master branch*/
+public class empWageProblem{
+	public static void main(String[] args){
+		/* Displaying welcome message in masterbranch. */
+      System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM.\n");
+	}
+}
