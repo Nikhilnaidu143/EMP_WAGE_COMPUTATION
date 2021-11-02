faf00584aa5e5264e2b608f5b7314dbd4aa89657
saitarun135 <saitarun135@gm.com>
2021-11-02 17:12:37 +0530

    UC-1:- Checked employee is present or absent

--- empWageProblem.java
+++ empWageProblem.java
@@ -1,7 +1,18 @@
-/*START:- displaying welcome mssg on master branch*/
-public class empWageProblem{
-	public static void main(String[] args){
-		/* Displaying welcome message in masterbranch. */
-      System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM.\n");
-	}
+/* UC-1 check employee is present or absent.*/
+public class empWageProblem {
+        public static void main(String[] args) {
+                /* Displaying welcome message in masterbranch. */
+                System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM.\n");
+
+                int isPresent = 1;
+                                         /*employee check */
+                double empCheck = Math.floor(Math.random()*10%2);
+
+                if(empCheck == isPresent) {
+                        System.out.println("EMPLOYEE IS PRESENT.");
+                }
+                else {
+                        System.out.println("EMPLOYEE IS ABSENT.");
+                }
+        }
 }
