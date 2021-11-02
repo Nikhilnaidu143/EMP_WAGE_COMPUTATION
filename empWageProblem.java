cc018134a25ae1ed524218e810c0422c3acd24c9
saitarun135 <saitarun135@gm.com>
2021-11-02 17:24:18 +0530

    UC-4:- Solved using Switch Case Statement.

--- empWageProblem.java
+++ empWageProblem.java
@@ -1,35 +1,36 @@
-/*UC-3:- Added parttime employee and calculated wage */
+/* UC-4:- Solve using switch case statementt. */
 public class empWageProblem {
-        public static void main(String[] args) {
-                /* Displaying welcome message in masterbranch. */
-                System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM.\n");
+   public static void main(String[] args) {
+      /* Displaying welcome message in masterbranch. */
+      System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM.\n");
 
-                /*Constant Variables.*/
-                int isFullTime = 2;
-                int isPartTime = 1;
-                int empRatePHr = 20;
+      /*Constant Variables.*/
+      final int isFullTime = 2;
+      final int isPartTime = 1;
+      final int empRatePHr = 20;
 
-                /*Temporary Variables */
-                int empHrs = 0;
-                int empWage = 0;
+      /*Temporary Variables */
+      int empHrs = 0;
+      int empWage = 0;
 
-                /* Finding employee is present or absent */
-                double empCheck = Math.floor(Math.random()*3);
-                if(empCheck == isFullTime) {
-                        empHrs = 8;
-                        System.out.println("FULLTIME EMPLOYEE... ");
-                }
-                else if (empCheck == isPartTime) {
-                        empHrs = 4;
-                        System.out.println("PARTTIME EMPLOYEE... ");
-                }
-                else{
-                        empHrs = 0;
-                        System.out.println("EMPLOYEE IS ABSENT... ");
-                }
-                /* Calculate employee daily wage */
-                empWage = empHrs * empRatePHr;
-                System.out.println("EMPLOYEE DAILY WAGE :- " + empWage + "\n");
-        }
+      /* Finding employee is present or absent */
+      int empCheck = (int) Math.floor(Math.random()*3);
+      switch (empCheck) {
+         case isFullTime:
+            empHrs = 8;
+            System.out.println("FULLTIME EMPLOYEE... ");
+            break;
+         case isPartTime:
+            empHrs = 4;
+            System.out.println("PARTTIME EMPLOYEE... ");
+            break;
+         default:
+            empHrs = 0;
+            System.out.println("EMPLOYEE IS ABSENT... ");
+         }
+      /* Calculate employee daily wage */
+      empWage = empHrs * empRatePHr;
+      System.out.println("EMPLOYEE DAILY WAGE :- " + empWage + "\n");
+   }
 }
 
