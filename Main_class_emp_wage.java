eb06f815610341a9e490f59ea99c8e9f4b0f4fd2
saitarun135 <nikhilnaidu473@gmail.com>
2021-11-11 08:43:34 +0530

    UC-7:- Applied OOPS Concept.

---
+++ Main_class_emp_wage.java
@@ -0,0 +1,8 @@
+// main class for UC-7
+
+class Main_class_emp_wage {
+   public static void main(String[] args) {
+      EmpWageProblem obj = new EmpWageProblem();
+      obj.computation_of_emp_wage();
+   }
+}
