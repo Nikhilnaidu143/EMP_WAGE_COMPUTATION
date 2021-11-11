c7431640c08a7fa5f285768a034949897ff2fe97
saitarun135 <nikhilnaidu473@gmail.com>
2021-11-11 06:16:16 +0530

    ADDED:- Part time employee and wage

--- EmpWageProblem.java
+++ EmpWageProblem.java
@@ -1,32 +1,38 @@
 
 /*UC-3:- Added parttime employee and calculated wage */
 public class EmpWageProblem {
-        public static void main(String[] args) {
-                /* Displaying welcome message in masterbranch. */
-                System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM.\n");
+	public static void main(String[] args) {
+	 	/* Displaying welcome message in masterbranch. */
+	   System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM.\n");
 
-                /*Constant Variables.*/
-                int isFullTime = 2;
-                int isPartTime = 1;
+      /*Constant Variables.*/
+    	int isFullTime = 2;
+      int isPartTime = 1;
+		int empRatePHr = 20;
 
-                double empCheck = Math.floor(Math.random()*3);
-                if(empCheck == isFullTime) {
-                        empHrs = 8;
-                        System.out.println("FULLTIME EMPLOYEE... ");
-                }
-                else if (empCheck == isPartTime) {
-                        empHrs = 4;
-                        System.out.println("PARTTIME EMPLOYEE... ");
-                }
-                else{
-                        empHrs = 0;
-                        System.out.println("EMPLOYEE IS ABSENT... ");
+		/*	Temporary variables */
+		int empHrs = 0;
+		int empWage = 0;
 
-                }
-                /* Calculate employee daily wage */
-                empWage = empHrs * empRatePHr;
-                System.out.println("EMPLOYEE DAILY WAGE :- " + empWage + "\n");
-        }
+
+     	double empCheck = Math.floor(Math.random()*3);
+      if(empCheck == isFullTime) {
+      	empHrs = 8;
+       	System.out.println("FULLTIME EMPLOYEE... ");
+   	}
+     	else if (empCheck == isPartTime) {
+      	empHrs = 4;
+        	System.out.println("PARTTIME EMPLOYEE... ");
+  		}
+     	else{
+         empHrs = 0;
+         System.out.println("EMPLOYEE IS ABSENT... ");
+
+     	}
+      /* Calculate employee daily wage */
+     	empWage = empHrs * empRatePHr;
+      System.out.println("EMPLOYEE DAILY WAGE :- " + empWage + "\n");
+	}
 }
 
 
