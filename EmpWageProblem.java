fff5ecfcb6352ae1b6ce1b4577513de3fb3454ef
saitarun135 <nikhilnaidu473@gmail.com>
2021-11-16 06:52:03 +0530

    UC-9 :- Ability to save total wage for each company is solved.

--- EmpWageProblem.java
+++ EmpWageProblem.java
@@ -1,15 +1,30 @@
-// UC-8:- calculate wages for multiple companies.
-public class EmpWageProblem {
-   public void computation_of_emp_wage(String company_name , int totalWorkDays , int maxHrsPMonth , int empRatePHr) {
+// UC-9 :- Ability to save total wage for each company.
 
-      /*Constant Variables.*/
-      final int isFullTime = 2;
-      final int isPartTime = 1;
+public class EmpWageProblem {
+	/*Constant Variables.*/
+    final int isFullTime = 2;
+    final int isPartTime = 1;
+    
+    final String company_name;
+    final int totalWorkDays;
+    final int maxHrsPMonth; 
+    final int empRatePHr;
+    
+    int totalEmpWage;
+    
+   public EmpWageProblem(String company_name, int totalWorkDays, int maxHrsPMonth, int empRatePHr) {
+		this.company_name = company_name;
+		this.totalWorkDays = totalWorkDays;
+		this.maxHrsPMonth = maxHrsPMonth;
+		this.empRatePHr = empRatePHr;
+	}
+
+   public int computation_of_emp_wage() {
 
       /*Temporary Variables */
       int empHrs = 0;
       int empWage = 0;
-      int totalEmpWage = 0;
+      
       int totalEmpHrs = 0;
       int numOfDays = 0;
 
@@ -32,6 +47,11 @@ public class EmpWageProblem {
          empWage = totalEmpHrs * empRatePHr;
          totalEmpWage = totalEmpWage + empWage;
       }
-      System.out.println(company_name + " employee total wage :- " + totalEmpWage + "\n");
+      return totalEmpWage;
+   }
+   
+   @Override
+   public String toString() {
+	   return company_name + " employee total wage :- " + totalEmpWage;
    }
 }
--- Main_class_emp_wage.java
+++ Main_class_emp_wage.java
@@ -1,17 +1,23 @@
-// main class for UC-7
-
 class Main_class_emp_wage {
-   public static void main(String[] args) {
-		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM.\n");
-      EmpWageProblem obj = new EmpWageProblem();
-
-		// calling method for company-1
-      obj.computation_of_emp_wage("BridgeLabz" , 20 , 100 , 40);
-
-		// calling method for company-2
-      obj.computation_of_emp_wage("Tech-Mahindra" , 22 , 90 , 30);
-
-		// calling method for company-3
-      obj.computation_of_emp_wage("Infosys" , 28 , 110 , 20);
-   }
+	public static void main(String[] args) {
+	   System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM.\n");
+	   
+	   // company-1
+	   EmpWageProblem BridgeLabz = new EmpWageProblem("BrideLabz" , 20 , 100 , 40);
+	   BridgeLabz.computation_of_emp_wage();
+	   
+	   System.out.println(BridgeLabz + "\n");
+	   
+	   //company-2
+	   EmpWageProblem TechMahindra = new EmpWageProblem("Tech-Mahindra", 22 , 90 , 30);
+	   TechMahindra.computation_of_emp_wage();
+	   
+	   System.out.println(TechMahindra + "\n");
+	   
+	   //company-3
+	   EmpWageProblem Infosys = new EmpWageProblem("Infosys", 22 , 90 , 30);
+	   Infosys.computation_of_emp_wage();
+	   
+	   System.out.println(Infosys + "\n");	   
+	}
 }
